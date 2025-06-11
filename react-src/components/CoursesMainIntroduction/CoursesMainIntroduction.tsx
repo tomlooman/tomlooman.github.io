574ce611c62733c9d828faaf0620dd64907bcd92
Naminek <nam.k.0519@gmail.com>
2025-06-11 20:34:49 +0200

    Add small frames in main intoroduction section

--- react-src/components/CoursesMainIntroduction/CoursesMainIntroduction.tsx
+++ react-src/components/CoursesMainIntroduction/CoursesMainIntroduction.tsx
@@ -6,7 +6,11 @@ const CoursesMainIntroduction = () => {
         <div className={style.container}>
             <div className={style.left} />
             <div className={style.middle} />
-            <div className={style.right} />
+            <div className={style.right}>
+                <div>Section 1</div>
+                <div>Section 2</div>
+                <div>Section 3</div>
+            </div>
         </div>
     )
 };
