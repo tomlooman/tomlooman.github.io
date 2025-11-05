4f0d65129781e6f4352680e2fbad705a002b5ec3
Naminek <nam.k.0519@gmail.com>
2025-11-05 13:44:07 +0100

    Add checkout page url to pricing signup button

--- react-src/src/components/Pricing/constants/Prices.ts
+++ react-src/src/components/Pricing/constants/Prices.ts
@@ -5,8 +5,10 @@ export const Prices = {
         INDIE_BASIC_DISCOUNTED: 295, // Change to null to remove discount
         INDIE_PAYMENT_PLAN: 85,
         INDIE_PAYMENT_PLAN_DISCOUNTED: 65,
+        INDIE_COUPON_CODE: null,
         PRO: 795,
-        PRO_DISCOUNTED: null
+        PRO_DISCOUNTED: null,
+        PRO_COUPON_CODE: null,
     },
     // Optimisation Course
     2: {
@@ -14,7 +16,23 @@ export const Prices = {
         INDIE_BASIC_DISCOUNTED: 295,
         INDIE_PAYMENT_PLAN: 85,
         INDIE_PAYMENT_PLAN_DISCOUNTED: 65,
+        INDIE_COUPON_CODE: null,
         PRO: 795,
-        PRO_DISCOUNTED: null
+        PRO_DISCOUNTED: null,
+        PRO_COUPON_CODE: null,
+    }
+};
+
+// product ids for the checkout page
+export const ProductId = {
+    1: {
+        indieBasic: "3423335",
+        indiePaymentPlan: "5880527",
+        pro: "3423338",
+    },
+    2: {
+        indieBasic: "5738003",
+        indiePaymentPlan: "5931155",
+        pro: "5738004",
     }
 };
