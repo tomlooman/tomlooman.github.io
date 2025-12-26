ea56ae70b88b92fcaf23e87fc2b894d4ee3e7318
Tom Looman <3748238+tomlooman@users.noreply.github.com>
2025-12-26 13:48:15 +0900

    Added Kit to email signup component

--- react-src/src/components/EmailSignUp/EmailSignUp.tsx
+++ react-src/src/components/EmailSignUp/EmailSignUp.tsx
@@ -4,17 +4,47 @@ import SingleInputForm from "../Forms/SigleInputForm";
 import style from "./EmailSignUp.module.scss";
 
 const EmailSignUp = () => {
-    const [email, setEmail] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    const submitEmail = async (email: string) => {
+        setLoading(true);
+
+        const formData = new FormData();
+        formData.append("email_address", email);
+
+        await fetch("https://app.kit.com/forms/8905753/subscriptions", {
+            method: "POST",
+            mode: "no-cors", // important for static sites
+            body: formData,
+        });
+
+        setLoading(false);
+        setSuccess(true);
+    };
 
     return (
         <div className={style.emailSignUpBanner}>
             <div className={style.emailSignUpContent}>
-                <b>Join thousands of game developers and get Unreal Engine C++ & Optimization insights in your inbox!</b>
-                {/* TODO: Change onSubmit to use a proper email signup function */}
-                <SingleInputForm placeholder="Enter your email" label="Email" onSubmit={(value) => console.log(value)} submitText="Sign me up" />
+                <b>
+                    Join thousands of game developers and get Unreal Engine C++ &
+                    Optimization insights in your inbox!
+                </b>
+
+                {success ? (
+                    <p>Check your email to confirm your subscription.</p>
+                ) : (
+                    <SingleInputForm
+                        placeholder="Enter your email"
+                        label="Email"
+                        onSubmit={submitEmail}
+                        submitText={loading ? "Submitting..." : "Sign me up"}
+                    />
+                )}
             </div>
         </div>
     );
-}
+};
+
 
 export default EmailSignUp;
