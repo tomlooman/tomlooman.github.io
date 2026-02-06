b148c9240852dc856df7e693211acdea97790c8a
Tom Looman <3748238+tomlooman@users.noreply.github.com>
2026-02-06 22:36:38 +0100

    fixed profile pictures for reviews

--- react-src/src/components/Reviews/constants/Reviews.ts
+++ react-src/src/components/Reviews/constants/Reviews.ts
@@ -9,7 +9,7 @@ export const reviews: { [key: string]: Review[] } = {
       jobTitle: "Staff Engineer",
       company: "Riot",
       text: "\"I think it's very rare to take a course that is 100% applicable to your professional work. After taking this course, I feel like I've accessed the matrix and downloaded the Unreal codex straight into my brain. Three months ago I had 0 Unreal knowledge and was really struggling to get my footing.<br /><br />Now, upon course completion, it's like a film has been lifted from my vision and I understand all the fundamentals of the engine that I'm required to work with on a day-to-day basis. I cannot overstate how grateful I am to have been able to take this course. Thank you Tom!\"",
-      imageUrl: "http://example.com/john.jpg"
+      imageUrl: "/assets/images/faces/anthony_profile.jpg"
     },
     {
       id: 2,
@@ -17,7 +17,7 @@ export const reviews: { [key: string]: Review[] } = {
       jobTitle: "Game Developer",
       company: "",
       text: "\"As a student studying game development and computer science, this course has been absolutely perfect for helping me bridge the gap between blueprinting and general C++ application programming. I'm grateful for the challenges within the assignments, the speed at which content is covered is perfect, and I haven't felt lost at any point within the course so far.<br />Additionally, the format of this course makes it perfect for self-pacing around my other university classes, which is greatly appreciated.\"",
-      imageUrl: "some_url.jpg"
+      imageUrl: "/assets/images/faces/jackson_profile.jpg"
     },
     {
       id: 3,
@@ -25,7 +25,7 @@ export const reviews: { [key: string]: Review[] } = {
       jobTitle: "PhD in Robotics",
       company: "",
       text: "\"As someone who had some prior experience with game development, I was initially concerned that the course might be basic for me. However, I was pleasantly surprised by the depth and quality of the content.<br /><br />The course managed to cover the fundamentals while delving into advanced topics that kept me engaged and eager to learn more.<br />Furthermore, the course shed light on those sometimes neglected but really useful details and features that may make a huge difference in efficient Unreal Engine development.\"",
-      imageUrl: "some_url.jpg"
+      imageUrl: "/assets/images/faces/mohamadshaaban.jpg"
     },
     {
       id: 4,
@@ -33,7 +33,7 @@ export const reviews: { [key: string]: Review[] } = {
       jobTitle: "Lead Engineer",
       company: "",
       text: "\"It was great! I needed something that could show me where to start, core concepts, and building blocks of Unreal with C++.<br />As a game developer who has years of experience with some proprietary game engines (C++) and Unity (C#), this lecture was spot on!\"",
-      imageUrl: "some_url.jpg"
+      imageUrl: "/assets/images/faces/yang_profile.jpg"
     },
     {
       id: 5,
@@ -41,7 +41,7 @@ export const reviews: { [key: string]: Review[] } = {
       jobTitle: "Game Developer",
       company: "",
       text: "\"Super useful! I already have +3 years of Unreal Engine experience only with Blueprints, and I know a little of C++ before this course, but the way that the instructor explains everything is super easy to follow!<br />I especially like the assignments, I think it's the best way to learn. 100% recommended!\"",
-      imageUrl: "some_url.jpg"
+      imageUrl: "/assets/images/faces/default_profile.jpg"
     },
     {
       id: 6,
@@ -49,7 +49,7 @@ export const reviews: { [key: string]: Review[] } = {
       jobTitle: ".Net Developer",
       company: "",
       text: "\"This is the first game dev tutorial I've found that finally caters to people who already have programming experience in other fields. This is exactly what I needed.\"",
-      imageUrl: "some_url.jpg"
+      imageUrl: "/assets/images/faces/jeroen_profile.png"
     }
   ],
   // Optimization Course
@@ -60,7 +60,7 @@ export const reviews: { [key: string]: Review[] } = {
       jobTitle: "Staff Engineer",
       company: "Riot",
       text: "\"I think it's very rare to take a course that is 100% applicable to your professional work. After taking this course, I feel like I've accessed the matrix and downloaded the Unreal codex straight into my brain. Three months ago I had 0 Unreal knowledge and was really struggling to get my footing.<br /><br />Now, upon course completion, it's like a film has been lifted from my vision and I understand all the fundamentals of the engine that I'm required to work with on a day-to-day basis. I cannot overstate how grateful I am to have been able to take this course. Thank you Tom!\"",
-      imageUrl: "http://example.com/john.jpg"
+      imageUrl: "/assets/images/faces/default_profile.jpg"
     },
     {
       id: 2,
@@ -68,7 +68,7 @@ export const reviews: { [key: string]: Review[] } = {
       jobTitle: "Game Developer",
       company: "",
       text: "\"As a student studying game development and computer science, this course has been absolutely perfect for helping me bridge the gap between blueprinting and general C++ application programming. I'm grateful for the challenges within the assignments, the speed at which content is covered is perfect, and I haven't felt lost at any point within the course so far.<br />Additionally, the format of this course makes it perfect for self-pacing around my other university classes, which is greatly appreciated.\"",
-      imageUrl: "some_url.jpg"
+      imageUrl: "/assets/images/faces/default_profile.jpg"
     }
   ]
 };
